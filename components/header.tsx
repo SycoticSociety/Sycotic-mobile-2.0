3e8da29ff51f846bd0d978f4d7e4bb7fd029f611
BrokeApeCronosClub <145580513+BrokeApeCronosClub@users.noreply.github.com>
2023-09-20 11:38:57 -0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
         <div className={styles.header}>
             <Image src={"/images/Thirdweb-Icon-Circle-Black-08 1.gif"} alt="" height={60} width={60}/>
             <p style={{ fontSize: 'larger' }}>
-                <a href="https://www.sycoticsociety.com" style={{ color: '#FFD700' }}>
+                <a href="https://www.sycoticsociety.com" style={{ color: '#000000' }}>
                     Broke Ape Cronos Club Mobile
                 </a>
             </p>
