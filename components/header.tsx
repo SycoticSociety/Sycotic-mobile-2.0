eba3ab83e521788a0c64d07760ec2c2441dbe3a9
BrokeApeCronosClub <145580513+BrokeApeCronosClub@users.noreply.github.com>
2023-09-20 13:31:13 -0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -6,8 +6,8 @@ export default function Header() {
         <div className={styles.header}>
             <Image src={"/images/Thirdweb-Icon-Circle-Black-08 1.gif"} alt="" height={60} width={60}/>
             <p style={{ fontSize: 'larger' }}>
-                <a href="https://www.sycoticsociety.com" style={{ color: '#000000' }}>
-                    Broke Ape Cronos Club Mobile
+                <a href="https://swap.crodex.app/#/swap?outputCurrency=0x3E68eA1588E32e51D98dBda6Ea05128513ECC713" style={{ color: '#000000' }}>
+                    Broke Ape Cronos Club Crodex!
                 </a>
             </p>
         </div>
