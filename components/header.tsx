30721afcf9167e72060248110d853c5945bd5014
BrokeApeCronosClub <145580513+BrokeApeCronosClub@users.noreply.github.com>
2023-09-20 14:57:13 -0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,15 @@
+import Link from 'next/link';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Header() {
     return (
         <div className={styles.header}>
-            <Image src={"/images/Thirdweb-Icon-Circle-Black-08 1.gif"} alt="" height={60} width={60}/>
+            <Link href="/">
+                <a>
+                    <Image src={"/images/Thirdweb-Icon-Circle-Black-08 1.gif"} alt="" height={60} width={60}/>
+                </a>
+            </Link>
             <p style={{ fontSize: 'larger' }}>
                 <a href="https://swap.crodex.app/#/swap?outputCurrency=0x3E68eA1588E32e51D98dBda6Ea05128513ECC713" style={{ color: '#000000' }}>
                     Broke Ape Cronos Club Crodex!
