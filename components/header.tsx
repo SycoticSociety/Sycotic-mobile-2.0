190ca430b0595f1caea59e570d58149db28a1e95
BrokeApeCronosClub <145580513+BrokeApeCronosClub@users.noreply.github.com>
2023-09-21 08:13:35 -0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -3,15 +3,17 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Header() {
+    const openLinkInNewWindow = (linkUrl: string) => {
+        window.open(linkUrl, "_blank", "width=800,height=600");
+    };
+
     return (
         <div className={styles.header}>
-            <Link href="/">
-                <a>
-                    <Image src={"/images/Thirdweb-Icon-Circle-Black-08 1.gif"} alt="" height={60} width={60}/>
-                </a>
-            </Link>
+            <a onClick={() => openLinkInNewWindow("https://swap.crodex.app/#/swap?outputCurrency=0x3E68eA1588E32e51D98dBda6Ea05128513ECC713")} style={{ cursor: 'pointer' }}>
+                <Image src={"/images/Thirdweb-Icon-Circle-Black-08 1.gif"} alt="" height={60} width={60}/>
+            </a>
             <p style={{ fontSize: 'larger' }}>
-                <a href="https://swap.crodex.app/#/swap?outputCurrency=0x3E68eA1588E32e51D98dBda6Ea05128513ECC713" style={{ color: '#000000' }}>
+                <a onClick={() => openLinkInNewWindow("https://swap.crodex.app/#/swap?outputCurrency=0x3E68eA1588E32e51D98dBda6Ea05128513ECC713")} style={{ color: '#000000', cursor: 'pointer' }}>
                     Broke Ape Cronos Club Crodex!
                 </a>
             </p>
