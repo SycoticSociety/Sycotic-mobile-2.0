634bdc11b89bff3560d2643290a6de2a34f5259e
BrokeApeCronosClub <145580513+BrokeApeCronosClub@users.noreply.github.com>
2023-09-21 07:58:26 -0400

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,62 +5,42 @@ import Link from "next/link";
 
 export default function Navbar() {
     const address = useAddress();
-
-    // Function to open external links in a new window
-    const openInNewWindow = (url: string) => {
-        window.open(url, "_blank");
-    };
-
     return (
         <div className={styles.navbar}>
-            <Link href="/cronos" passHref>
-                <a className={styles.navIcon}>
-                    <Image src={"/cronos-icon.png"} alt="" width={31} height={41} />
-                    <p className={styles.navIconLabel}>Cronos</p>
-                </a>
+            <Link href="https://cronoscan.com/token/0x3e68ea1588e32e51d98dbda6ea05128513ecc713" className={styles.navIcon}>
+                <Image src={"/cronos-icon.png"} alt="" width={31} height={41}/>
+                <p className={styles.navIconLabel}>Cronos</p>
             </Link>
 
-            <Link href="/liquidity" passHref>
-                <a className={styles.navIcon}>
-                    <Image src={"/dexScreener.png"} alt="" width={46} height={46} />
-                    <p className={styles.navIconLabel}>Liquidity</p>
-                </a>
+              <Link href="https://dexscreener.com/cronos/0x9fb3fa7be9ca04efdfb43d8cd4ed38752187aaf5" className={styles.navIcon}>
+                <Image src={"/dexScreener.png"} alt="" width={46} height={46}/>
+                <p className={styles.navIconLabel}>Liquidity</p>
             </Link>
 
-            <Link href="/twitter" passHref>
-                <a className={styles.navIcon}>
-                    <Image src={"/claim-icon.png"} alt="" width={46} height={46} />
-                    <p className={styles.navIconLabel}>Twitter</p>
-                </a>
+             <Link href="https://x.com/BrokeApeCronos?s=20" className={styles.navIcon}>
+                <Image src={"/claim-icon.png"} alt="" width={46} height={46}/>
+                <p className={styles.navIconLabel}>Twitter</p>
             </Link>
-
-            <a
-                href="https://broke-ape-token-mint.vercel.app/"
-                className={styles.navIcon}
-                onClick={(e) => {
-                    e.preventDefault();
-                    openInNewWindow("https://broke-ape-token-mint.vercel.app/");
-                }}
-            >
-                <Image src={"/nft-icon.png"} alt="" width={40} height={40} />
+            
+            <Link href="https://broke-ape-token-mint.vercel.app/" className={styles.navIcon}>
+                <Image src={"/nft-icon.png"} alt="" width={40} height={40}/>
                 <p className={styles.navIconLabel}>Mint Now</p>
-            </a>
-
+            </Link>
             <div className={styles.navIcon}>
-                {address && (
-                    <ConnectWallet
-                        btnTitle="Login"
-                        detailsBtn={() => {
-                            return (
-                                <div>
-                                    <Image src={"/profile-icon.png"} alt="" width={40} height={40} />
-                                    <p className={styles.navIconLabel}>Profile</p>
-                                </div>
-                            );
-                        }}
-                    />
-                )}
+            {address && (
+                <ConnectWallet
+                    btnTitle="Login"
+                    detailsBtn={() => {
+                        return (
+                            <div>
+                                <Image src={"/profile-icon.png"} alt="" width={40} height={40}/>
+                                <p className={styles.navIconLabel}>Profile</p>
+                            </div>
+                        )
+                    }}
+                />
+            )}
             </div>
         </div>
-    );
+    )
 }
