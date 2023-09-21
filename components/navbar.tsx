5397ecebdf08ec10d3d0526b37c8c298335104bc
BrokeApeCronosClub <145580513+BrokeApeCronosClub@users.noreply.github.com>
2023-09-21 07:26:15 -0400

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,31 +1,30 @@
 import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Navbar() {
     const address = useAddress();
     return (
         <div className={styles.navbar}>
-            <Link href="https://cronoscan.com/token/0x3e68ea1588e32e51d98dbda6ea05128513ecc713" className={styles.navIcon}>
+            <a href="https://cronoscan.com/token/0x3e68ea1588e32e51d98dbda6ea05128513ecc713" target="_blank" className={styles.navIcon}>
                 <Image src={"/cronos-icon.png"} alt="" width={31} height={41}/>
                 <p className={styles.navIconLabel}>Cronos</p>
-            </Link>
+            </a>
 
-              <Link href="https://dexscreener.com/cronos/0x9fb3fa7be9ca04efdfb43d8cd4ed38752187aaf5" className={styles.navIcon}>
+            <a href="https://dexscreener.com/cronos/0x9fb3fa7be9ca04efdfb43d8cd4ed38752187aaf5" target="_blank" className={styles.navIcon}>
                 <Image src={"/dexScreener.png"} alt="" width={46} height={46}/>
                 <p className={styles.navIconLabel}>Liquidity</p>
-            </Link>
+            </a>
 
-             <Link href="https://x.com/BrokeApeCronos?s=20" className={styles.navIcon}>
+            <a href="https://x.com/BrokeApeCronos?s=20" target="_blank" className={styles.navIcon}>
                 <Image src={"/claim-icon.png"} alt="" width={46} height={46}/>
                 <p className={styles.navIconLabel}>Twitter</p>
-            </Link>
+            </a>
             
-            <Link href="/" className={styles.navIcon}>
+            <a href="https://broke-ape-token-mint.vercel.app/" target="_blank" className={styles.navIcon}>
                 <Image src={"/nft-icon.png"} alt="" width={40} height={40}/>
-                <p className={styles.navIconLabel}>Claim</p>
-            </Link>
+                <p className={styles.navIconLabel}>Mint Now</p>
+            </a>
             <div className={styles.navIcon}>
             {address && (
                 <ConnectWallet
