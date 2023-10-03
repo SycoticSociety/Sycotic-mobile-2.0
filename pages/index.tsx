3e0c94b51a828f62ba9241408a04cde546bfa470
BrokeApeCronosClub <145580513+BrokeApeCronosClub@users.noreply.github.com>
2023-10-02 20:35:01 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,25 +20,10 @@ const Home: NextPage = () => {
 
   return (
     <div className={styles.container}>
-      {address ? (
-        <div className={styles.nftClaim}>
-          <MediaRenderer
-            src={contractMetadata?.image}
-            width="auto"
-            height="50%"
-            style={{
-              borderRadius: "20px",
-              maxWidth: "600px",
-            }}
-          />
-          <h1>{contractMetadata?.name}</h1>
-          <ConnectWallet btnTitle="Login" className={styles.connectWalletButton} /> {/* Apply the CSS class here */}
-        </div>
-      ) : (
-        <div className={styles.loginContainer}>
-          <ConnectWallet btnTitle="Login" className={styles.connectWalletButton} /> {/* Apply the CSS class here */}
-        </div>
-      )}
+
+
+
+      
     </div>
   );
 };
