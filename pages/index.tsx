6950c8771fbaff138e3bd58f543e441607685789
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-10-12 16:34:23 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
     <div className={styles.container} style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
       <a href="#" onClick={handleImageClick}>
         <div style={{ display: "grid", placeItems: "center", width: "100%", height: "100%" }}>
-          <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", marginTop: "50px" }}>
+          <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", marginTop: "125px" }}>
             <img
               src="/images/Thirdweb-Icon-Circle-Black-08 1 (3).gif" // Replace with your image URL
               alt="Image Description"
