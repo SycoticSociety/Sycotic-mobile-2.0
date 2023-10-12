2bba7f8ad6ba170aa663dd5899a88a52b91e66b9
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-10-11 23:29:57 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,15 +16,14 @@ const Home: NextPage = () => {
   const { contract } = useContract(NFT_CONTRACT_ADDRESS);
   const { data: contractMetadata } = useContractMetadata(contract);
 
- const handleImageClick = () => {
+  const handleImageClick = () => {
     window.open("https://swap.crodex.app/#/swap?outputCurrency=0x4E2E7700C8f8A414b4AF8828f54330a304B6625d", "_blank", "width=700,height=600");
-};
-
+  };
 
   return (
     <div className={styles.container} style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
       <a href="#" onClick={handleImageClick}>
-        <div style={{ position: "relative", width: "35%", height: "35%", left: "165px" }}> {/* Added left: "40px" */}
+        <div style={{ position: "relative", width: "35%", height: "35%", left: "165px", marginTop: "60px" }}> {/* Added marginTop */}
           <img
             src="/images/Thirdweb-Icon-Circle-Black-08 1 (3).gif" // Replace with your image URL
             alt="Image Description"
