13df5d968fedcc0177cc2ec76fb7afecac1300ff
Sycotic Society <thecrazydutchman2000@gmail.com>
2024-01-17 16:15:41 -0500

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
             </a>
             <p style={{ fontSize: 'larger' }}>
                 <a onClick={() => openLinkInNewWindow("https://swap.crodex.app/#/swap?outputCurrency=0xE7F3c7C6611357313B5C2734341fe9cBad1D9f59 ")} style={{ color: '#000000', cursor: 'pointer' }}>
-                     💥Sycotic💢Society💥 🔥🔥 🚒 
+                     🔥Sycotic💢Society🔥 🚒 
                 </a>
             </p>
         </div>
